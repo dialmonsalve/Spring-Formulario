48c00275bdbb620fa8d25af6f5ac6431a373968f
Diego Alejandro <diales1981@gmail.com>
2022-07-22 21:00:59 -0500

    Creación del proyecto de formulario con validaciones

---
+++ src/main/java/com/diamones/springboot/app/SpringBootMiPrimerFormularioApplication.java
@@ -0,0 +1,13 @@
+package com.diamones.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMiPrimerFormularioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMiPrimerFormularioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diamones/springboot/app/SpringBootMiPrimerFormularioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diamones.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMiPrimerFormularioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
