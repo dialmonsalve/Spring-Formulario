812c210dbcabec1c55867e83ac68b3808e6151fd
Diego Alejandro <diales1981@gmail.com>
2022-07-24 20:58:52 -0500

    Realizacion de plantilla html

--- src/main/java/com/diamones/springboot/app/Controllers/FormController.java
+++ src/main/java/com/diamones/springboot/app/Controllers/FormController.java
@@ -6,11 +6,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.diamones.springboot.app.models.Usuario;
 
 @Controller
 public class FormController {
+	
+	@RequestMapping({"/", "/index", " "})
+	public String index() {
+		
+		return "/layout/layout";
+	}
 	
 	@GetMapping("/form")
 	public String crearFormulario(@Valid Usuario usuario, Model model) {
@@ -27,5 +34,7 @@ public class FormController {
 		
 		return "resultado";
 	}
+	
+	
 
 }
